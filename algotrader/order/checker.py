9b8109a882984bfbabe43752212b330a29fe670b
Omer Murat Yildirim <omermuratyildirim@gmail.com>
2018-12-22 17:45:33 +0300

    Refactor order checker

--- algotrader/order/checker.py
+++ algotrader/order/checker.py
@@ -22,10 +22,11 @@ class OrderChecker():
         self.exchange_adapter = adapter_cls()
 
     def check_orders(self):
-        statuses = ['pending', 'open']  # These should be constant and global.
+        statuses = ['pending', 'open']  # TODO: These should be constant and global.
         orders = self.storage.get_orders(statuses)
 
         for order in orders:
+            # TODO: order_result['id'] already in db, decrease API calls.
             order_result = self.excchange_adapter.get_order(order.id)
             coinbase_fills = self.exchange_adapter.get_fills(order_result['id'])
             persisted_fills = order.get('fills')
@@ -35,15 +36,13 @@ class OrderChecker():
                 continue
 
             logger.info('New Fills: ' + str(new_fills))
-            self.storage.update(order.id, new_fills, order.status)  # TOOD: order.status or order_result status?
+            self.storage.update(order.id, new_fills, order.status)  # TODO: order.status or order_result status?
 
-    def _get_different_fills(self, coinbase_fills, persisted_fills=[]):
+    def _get_new_fills(self, coinbase_fills: list, persisted_fills: list):
         """
         Find all different fill objects by comparing two lists where one is from coinbase and the other one is from db.
         """
-        diff_fills = []
-        for coinbase_fill in coinbase_fills:
-            for persisted_fill in persisted_fills:
-                if coinbase_fill['trade_id'] == persisted_fill['trade_id']:
-                    diff_fills.append(coinbase_fill)
-        return diff_fills
+        exchange_fills = set(coinbase_fills)
+        db_fills = set(persisted_fills)
+
+        return list(exchange_fills.difference(db_fills))
