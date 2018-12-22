a8b8fcc8c1333953edf9c1c523fc313120520a65
Omer Murat Yildirim <omermuratyildirim@gmail.com>
2018-12-22 17:49:34 +0300

    Rename

--- algotrader/order/checker.py
+++ algotrader/order/checker.py
@@ -3,7 +3,6 @@ from algotrader.exchange.coinbase.adapter import CoinbaseAdapter
 from algotrader.storage.manager import StorageManager
 
 
-# TODO: Get rid of coinbase specific code.
 class OrderChecker():
 
     def __init__(self, storage: StorageManager, exchange):
@@ -28,9 +27,9 @@ class OrderChecker():
         for order in orders:
             # TODO: order_result['id'] already in db, decrease API calls.
             order_result = self.exchange_adapter.get_order(order.id)
-            coinbase_fills = self.exchange_adapter.get_fills(order_result['id'])
+            exchange_fills = self.exchange_adapter.get_fills(order_result['id'])
             persisted_fills = order.get('fills')
-            new_fills = self._get_different_fills(coinbase_fills, persisted_fills)
+            new_fills = self._get_different_fills(exchange_fills, persisted_fills)
 
             if not new_fills:
                 continue
@@ -38,11 +37,11 @@ class OrderChecker():
             logger.info('New Fills: %s', new_fills)
             self.storage.update(order.id, new_fills, order.status)  # TODO: order.status or order_result status?
 
-    def _get_new_fills(self, coinbase_fills: list, persisted_fills: list):
+    def _get_new_fills(self, exchange_fills: list, persisted_fills: list):
         """
-        Find all different fill objects by comparing two lists where one is from coinbase and the other one is from db.
+        Find all different fill objects by comparing two lists where one is from exchange and the other one is from db.
         """
-        exchange_fills = set(coinbase_fills)
+        exchange_fills = set(exchange_fills)
         db_fills = set(persisted_fills)
 
         return list(exchange_fills.difference(db_fills))
